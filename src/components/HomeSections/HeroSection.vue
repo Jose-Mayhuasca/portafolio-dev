4413b97a537e38d073320e12efe75ab216850ee6
Jose-Mayhuasca <josedanielmayhuasca@gmail.com>
2025-07-24 00:39:50 -0500

    feat: hero section complete

--- src/components/HomeSections/HeroSection.vue
+++ src/components/HomeSections/HeroSection.vue
@@ -1,7 +1,18 @@
 <template>
-    <div>
-        <h1>
-            HOLA
-        </h1>
-    </div>
+    <section class="heroSection">
+        <div class="conteinerBg">
+            <video autoplay loop muted class="backgroundVideo">
+                <source src="/src/assets/videos/bgHeroHome.mp4" type="video/mp4">
+                Tu navegador no soporta videos HTML5.
+            </video>
+            <div class="content">
+                <h1 class="regular colorWhite">Cada <span class="boldItalic">linea de codigo</span> cuenta. <br> Cada
+                    <span class="boldItalic">detalle</span> importa.
+                </h1>
+            </div>
+            <div class="button">
+                <Button label="¡Impulsa tu idea!" class="cta" />
+            </div>
+        </div>
+    </section>
 </template>
--- src/components/HomeSections/NavbarSection.vue
+++ src/components/HomeSections/NavbarSection.vue
@@ -1,7 +1,9 @@
 <template>
-    <div class="card">
-        <Menubar :model="items" />
-    </div>
+    <section class="navbarSection">
+        <div class="conteinerNavbar">
+            <Menubar :model="items" class="navbar regular superSmall" />
+        </div>
+    </section>
 </template>
 
 <script setup>
@@ -9,47 +11,19 @@ import { ref } from "vue";
 
 const items = ref([
     {
-        label: 'Home',
+        label: 'Inicio',
         icon: 'pi pi-home'
     },
     {
-        label: 'Features',
+        label: 'Sobre Mi',
         icon: 'pi pi-star'
     },
     {
-        label: 'Projects',
-        icon: 'pi pi-search',
-        items: [
-            {
-                label: 'Components',
-                icon: 'pi pi-bolt'
-            },
-            {
-                label: 'Blocks',
-                icon: 'pi pi-server'
-            },
-            {
-                label: 'UI Kit',
-                icon: 'pi pi-pencil'
-            },
-            {
-                label: 'Templates',
-                icon: 'pi pi-palette',
-                items: [
-                    {
-                        label: 'Apollo',
-                        icon: 'pi pi-palette'
-                    },
-                    {
-                        label: 'Ultima',
-                        icon: 'pi pi-palette'
-                    }
-                ]
-            }
-        ]
+        label: 'Proyectos',
+        icon: 'pi pi-search'
     },
     {
-        label: 'Contact',
+        label: 'Contacto',
         icon: 'pi pi-envelope'
     }
 ]);
