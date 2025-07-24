d3bf39c45d541616f1e521bf65d4e04a797af0c1
Jose-Mayhuasca <josedanielmayhuasca@gmail.com>
2025-07-23 22:16:53 -0500

    feat: segundo commit - configuracion theme components primevue

--- src/main.js
+++ src/main.js
@@ -1,23 +1,45 @@
+// Vue & App
 import { createApp } from 'vue'
 import App from './App.vue'
+
+// Router
 import router from './router'
+
+// PrimeVue core
 import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+
+// PrimeVue components (registro modular)
 import Componentes from './primevue'
+
+// PrimeIcons
 import 'primeicons/primeicons.css'
+
+// Estilos globales personalizados
 import '@/assets/styles.scss'
 
-// const app = createApp(App)
-
-// app.use(router)
-
-// app.mount('#app')
-
+// Inicialización de la app
 const app = createApp(App)
-app.use(PrimeVue, { inputStyle: 'filled' })
+
+// Configuración del tema Styled con Aura
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      prefix: 'p',
+      darkModeSelector: 'system',
+      cssLayer: false,
+    },
+  },
+})
+
+// Router
 app.use(router)
 
+// Registro de componentes PrimeVue
 Object.entries(Componentes).forEach(([name, component]) => {
   app.component(name, component)
 })
 
+// Montaje de la app
 app.mount('#app')
