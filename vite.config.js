a697160258613d23fec0d0f96b706aca2be304ea
Jose-Mayhuasca <josedanielmayhuasca@gmail.com>
2025-07-26 15:36:04 -0500

    Fix: configurar base en vite.config.js para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portafolio-dev/',
+  base: '/portafolio-dev/', // ✅ Correcto: coincide con el nombre de tu repo
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
