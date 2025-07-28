dbebaeb8efdf1d90aab208646613eba107b4810e
Jose-Mayhuasca <josedanielmayhuasca@gmail.com>
2025-07-28 02:26:54 -0500

    feat: fix - espacio blanco

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,22 +3,35 @@ import NavbarSection from '../components/HomeSections/NavbarSection.vue';
 import HeroSection from '../components/HomeSections/HeroSection.vue';
 import PresentationSection from '../components/HomeSections/PresentationSection.vue';
 import AboutMeSection from '../components/HomeSections/AboutMeSection.vue';
-import TecnologiesSection from '../components/HomeSections/TecnologiesSection.vue';
 import ProjectsSection from '../components/HomeSections/ProjectsSection.vue';
 import ContactSection from '../components/HomeSections/ContactSection.vue';
 import FooterSection from '../components/HomeSections/FooterSection.vue';
+import TechnologiesSection from '../components/HomeSections/TechnologiesSection.vue';
 
 </script>
 
 <template>
     <NavbarSection />
-    <div style="display: flex; flex-direction: column; gap: 7.875rem;">
+    <div class="homepage">
         <HeroSection />
         <PresentationSection />
         <AboutMeSection />
-        <TecnologiesSection />
+        <TechnologiesSection />
         <ProjectsSection />
         <ContactSection />
         <FooterSection />
     </div>
 </template>
+
+<style scoped>
+.homepageContainer {
+    width: 100%;
+    height: auto;
+}
+
+.homepage {
+    display: flex;
+    flex-direction: column;
+    gap: 7.875rem;
+}
+</style>
